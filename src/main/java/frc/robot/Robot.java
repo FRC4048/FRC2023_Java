2e430e2420aedf29cc0b25584a18841186e9a25e
nadarnic25 <82886063+nadarnic25@users.noreply.github.com>
2023-01-14 12:48:37 -0500

    made autoChooser and added it to robotInit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,7 +18,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-
+  public AutoChooser autoChooser = new AutoChooser();
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -28,6 +28,8 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    autoChooser.addOptions();
+    autoChooser.initialize();
   }
 
   /**
