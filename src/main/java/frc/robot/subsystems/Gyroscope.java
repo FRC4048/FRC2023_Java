e72056076839aa94cb2f5d8ab71c109f163c5477
QuokkaLord <timofey.tylik@gmail.com>
2023-02-11 15:52:43 -0500

    Added Field2d

--- src/main/java/frc/robot/subsystems/Gyroscope.java
+++ src/main/java/frc/robot/subsystems/Gyroscope.java
@@ -2,12 +2,21 @@ package frc.robot.subsystems;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.SmartShuffleboard;
 
 public class Gyroscope extends SubsystemBase {
     AHRS gyroscope = new AHRS();
-    public Gyroscope() {}
+    private final Field2d robotField = new Field2d();
+
+    public Gyroscope() {
+        SmartDashboard.putData(robotField);
+    }
 
     public void periodic() {
         double displacementX = gyroscope.getDisplacementX();
@@ -17,6 +26,13 @@ public class Gyroscope extends SubsystemBase {
         SmartShuffleboard.put("Odometry", "Pannels", "Y Change", displacementY);
 
         double gyroAngle = gyroscope.getAngle();
+        double convertToRadians = gyroAngle * (Math.PI/180);
+        Rotation2d rotation = new Rotation2d(convertToRadians);
         SmartShuffleboard.put("Odometry", "Pannels", "Gyro Angle", gyroAngle);
+
+        //robotField.setRobotPose(displacementX, displacementY, rotation);
+        Pose2d pose = new Pose2d(displacementX, displacementY, rotation);
+        robotField.setRobotPose(pose);
+        
     }
 }
