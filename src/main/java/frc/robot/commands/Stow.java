a77b4bb74fcce77a4d3d0b2d5f06e517e9185720
Sahiltheram <sahil.a.thadani@gmail.com>
2023-02-21 12:33:14 -0500

    Stow Finished- Testing.

--- src/main/java/frc/robot/commands/Stow.java
+++ src/main/java/frc/robot/commands/Stow.java
@@ -1,16 +1,20 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.extender.ExtendToPosition;
 import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Extender;
 import frc.robot.subsystems.GripperSubsystem;
 
 public class Stow extends SequentialCommandGroup {
 
-    public Stow (Arm arm, GripperSubsystem gripper){
+    public Stow (Arm arm, GripperSubsystem gripper, Extender extender){
         addCommands(
-                new MoveArm(0, arm),
-                new MoveGripper(gripper)
-
+                new OpenGripper(gripper),
+                new Wait(10),
+                new ExtendToPosition(extender, 0),
+                new Wait (10),
+                new SetArmAngle(arm, 0)
         );
     }
 
--- src/main/java/frc/robot/commands/Wait.java
+++ src/main/java/frc/robot/commands/Wait.java
@@ -1,30 +1,30 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class MoveExtender extends CommandBase {
-
-    MoveExtender(){
-
+public class Wait extends CommandBase {
+    private double time;
+    private double startTime;
+    public Wait (double time) {
+        this.time = time;
     }
     @Override
     public void end(boolean interrupted) {
-
     }
 
     @Override
     public void initialize() {
-
-
+        startTime = Timer.getFPGATimestamp();
     }
 
     @Override
     public void execute(){
-
     }
+
 
     @Override
     public boolean isFinished() {
-        return false;
+        return (Timer.getFPGATimestamp() - startTime) >= time;
     }
 }
