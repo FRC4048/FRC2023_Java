f2e7cfd2fa0128835208ddd61eb292739c5b4445
Matvey Didenko <matvey.s.didenko@gmail.com>
2023-02-24 17:15:59 -0500

    AutoBalance function with robot acceleration. needs testing

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -1,59 +1,49 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.utils.SmartShuffleboard;
 
-public class AutoBalance extends CommandBase {
-  /** Creates a new AutoBalance. */
-  Drivetrain drivetrain;
-  double speedx, speedy;
-  double filterx, filtery;
-  public AutoBalance(Drivetrain drivetrain) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.drivetrain = drivetrain;
-  }
+public class AutoBalance extends CommandBase{
+    Drivetrain drivetrain;
+    double accelX;
+    double accelY;
+    double speedX;
+    double speedY;
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    filterx = drivetrain.getAccelX();
-    filtery = drivetrain.getAccelY();
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    speedx = 0;
-    speedy= 0;
-    filterx = filterx + (filterx + drivetrain.getAccelX())/3;
-    filtery = filtery + (filtery + drivetrain.getAccelY())/5;
-    SmartShuffleboard.put("Auto Balance", "Filter x", drivetrain.getAccelX());
-    SmartShuffleboard.put("Auto Balance", "Filter y", drivetrain.getAccelY());
-    if (Math.abs(filterx) > 0.05) {
-      speedx = Math.signum(filterx) * 0.1;
+    public AutoBalance(Drivetrain drivetrain, double accelX, double accelY){
+        this.drivetrain = drivetrain;
+        this.accelX = accelX;
+        this.accelY = accelY;
     }
 
-    if (Math.abs(filtery) > 0.05) {
-      speedy = Math.signum(filtery) * 0.1;
+    public double accelToSpeed(double accel) //converting acceleration value we get into speed (EXACT VALUES UNKOWN)
+    {
+        return accel * 0.1;
     }
 
-    drivetrain.drive(speedx, 0, 0, true);
-  }
+        // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    drivetrain.drive(0, 0, 0, true);
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        while(!((accelX < 0.1 && accelX > -0.1) || (accelY < 0.1 && accelY > -0.1)))// while the robot is NOT balanced (EXACT ACCELERATION BOUNDARIES UNKNOWN)
+        {
+            speedX = accelToSpeed(accelX);
+            speedY = accelToSpeed(accelY);
+            drivetrain.drive(speedX, speedY, 0, true);
+        }
+        drivetrain.drive(0, 0, 0, true);
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return Math.abs(filterx) < 0.1 & Math.abs(filtery) < 0.1;
-  }
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {}
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return (accelX == 0 && accelY == 0);
+    }
 }
