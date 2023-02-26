cdcb95d6163f9244bfa910c278fbd0e4d567f88d
goatfanboi23 <python.goat@gmail.com>
2023-02-25 19:37:17 -0500

    chnage move sequence to extend and move arm at the same time (untested)

--- src/main/java/frc/robot/commands/arm/ArmMoveSequence.java
+++ src/main/java/frc/robot/commands/arm/ArmMoveSequence.java
@@ -13,13 +13,13 @@ import frc.robot.subsystems.PieceGrid;
 /**
  * DO NOT CALL THIS DIRECTLY MUST BE WRAPPED IN A COMMAND
  */
-public class ArmMoveSequence extends SequentialCommandGroup {
+public class ArmMoveSequence extends ParallelCommandGroup {
     public ArmMoveSequence(Arm arm, Extender extender, double armTargetPosition, double extenderTargetPosition) {
         addCommands(
-        new VoltageMoveArm(arm, Constants.ARM_AUTO_VOLTAGE, armTargetPosition),
-        new ParallelCommandGroup(
-                new HoldArmPID(arm,armTargetPosition),
-                new ExtendToPosition(extender,extenderTargetPosition))
+            new SequentialCommandGroup(
+                new VoltageMoveArm(arm, Constants.ARM_AUTO_VOLTAGE, armTargetPosition), 
+                new HoldArmPID(arm,armTargetPosition)
+            ),new ExtendToPosition(extender,extenderTargetPosition)
         );
     }
 
