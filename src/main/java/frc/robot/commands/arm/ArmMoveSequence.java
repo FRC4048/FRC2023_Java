48c310f14aa7c6c3400f4e12a5e78fdc0ccf2d15
goatfanboi23 <python.goat@gmail.com>
2023-03-01 21:43:37 -0500

    worked on getting better protections

--- src/main/java/frc/robot/commands/arm/ArmMoveSequence.java
+++ src/main/java/frc/robot/commands/arm/ArmMoveSequence.java
@@ -1,5 +1,7 @@
 package frc.robot.commands.arm;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.ArmPositionGrid;
@@ -9,6 +11,8 @@ import frc.robot.commands.extender.ExtendToPosition;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Extender;
 import frc.robot.subsystems.PieceGrid;
+import frc.robot.subsystems.ProtectionMechanism;
+import frc.robot.utils.SmartShuffleboard;
 
 /**
  * DO NOT CALL THIS DIRECTLY MUST BE WRAPPED IN A COMMAND
@@ -19,7 +23,18 @@ public class ArmMoveSequence extends ParallelCommandGroup {
             new SequentialCommandGroup(
                 new VoltageMoveArm(arm, Constants.ARM_AUTO_VOLTAGE, armTargetPosition), 
                 new HoldArmPID(arm,armTargetPosition)
-            ),new ExtendToPosition(extender,extenderTargetPosition)
+            ),new ExtendToPosition(extender,extenderTargetPosition),
+                new CommandBase(){
+            @Override
+            public void execute() {
+                SmartShuffleboard.put("Arm","estAngle", ProtectionMechanism.angleFromEncoder(arm.getEncoderValue()));
+                SmartShuffleboard.put("Arm","extMax", ProtectionMechanism.maxEnc(arm.getEncoderValue()));
+            }
+            @Override
+            public boolean isFinished() {
+                return  true;
+            }
+        }
         );
     }
 
