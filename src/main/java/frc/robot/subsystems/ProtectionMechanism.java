28caa8b3cb9bafd565037d2b78f6c48b5781b26f
goatfanboi23 <python.goat@gmail.com>
2023-03-01 21:43:37 -0500

    worked on getting better protections

--- src/main/java/frc/robot/subsystems/ProtectionMechanism.java
+++ src/main/java/frc/robot/subsystems/ProtectionMechanism.java
@@ -20,20 +20,40 @@ public final class ProtectionMechanism extends SubsystemBase {
          if(Constants.DEBUG){
               SmartShuffleboard.put("DEBUG","CanExtend",safeToExtend());
               SmartShuffleboard.put("DEBUG","CanLowerArm",safeToLowerArm());
-              SmartShuffleboard.put("DEBUG","CanZeroArm",safeToZeroArm());
               SmartShuffleboard.put("DEBUG","CanOpenGripper",safeToOpenGripper());
          }
      }
 
      private boolean safeToExtend(){
+          if (arm.getEncoderValue() > 25) return true;
+          //double extenderMax =  heightOfArm/Math.cos(armAngle)
+          //return (arm.getEncoderValue() < extenderMax && arm.getEncoderValue() > Constants.NO_EXTENSION_ZONE)
           return arm.getEncoderValue() > Constants.NO_EXTENSION_ZONE;
      }
      public boolean safeToLowerArm(){
-          return !(arm.getEncoderValue() < Constants.GRIP_NEEDS_CLOSE_ZONE && (gripper.getopenLimitSwitch())) && !(safeToExtend() && extender.getExtenderSensorPos() > Constants.NO_ARM_LOWER_ZONE);
+          int heightOfArm = 47;
+          double armAngle = angleFromEncoder(arm.getEncoderValue());
+          double extenderMax =  heightOfArm/Math.cos(armAngle);
+          if (arm.getEncoderValue() > 25) return true;
+          if (arm.getEncoderValue() < Constants.GRIP_NEEDS_CLOSE_ZONE && gripper.getopenLimitSwitch()) return false;
+          return (extender.getExtenderSensorPos() < extenderMax);
+
      }
-     public boolean safeToZeroArm(){
-          return !gripper.getopenLimitSwitch() && safeToLowerArm();
+     public static double angleFromEncoder(double value){
+          return -17.3 + (0.672 * value) + (-.00453 * Math.pow(value,2)) + (-.00328 * Math.pow(value,3)) + (.0000583 * Math.pow(value,4));
+          //17.3 + 0.672x + -4.53E-03x^2 + 3.28E-03x^3 + -5.83E-05x^4
      }
+
+     /**
+      * @param value arm encoder value
+      * @return the max distance in inches of extender
+      */
+     public static double maxEnc(double value){
+          int heightOfArm = 47;
+          double armAngle = angleFromEncoder(value) *  Math.PI/180;
+          return heightOfArm/Math.cos(armAngle);
+     }
+     
      public boolean safeToOpenGripper(){
           return arm.getEncoderValue() > Constants.GRIP_NEEDS_CLOSE_ZONE;
      }
