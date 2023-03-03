e74384ab0b856bbf8463de8ee680a0eaa3ed7099
bengold22 <98672229+bengold22@users.noreply.github.com>
2023-03-02 22:04:32 -0500

    PhotonVision Implementation

--- src/main/java/frc/robot/subsystems/PhotonCameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonCameraSubsystem.java
@@ -2,12 +2,10 @@ package frc.robot.subsystems;
 
 import java.util.Optional;
 
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
-import org.photonvision.PhotonUtils;
-import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
@@ -17,116 +15,119 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.apriltags.AprilTagMap;
 import frc.robot.utils.SmartShuffleboard;
 
 public class PhotonCameraSubsystem extends SubsystemBase {
 
+  private static final String FMSINFO_TABLE = "FMSInfo";
+  private static final String IS_RED_ALLIANCE = "IsRedAlliance";
+
   private PhotonCamera camera;
   private AprilTagFieldLayout layout;
-  private Pose2d builtIn2D;
+  private Pose2d robotFieldPose;
   PhotonPoseEstimator estimator;
-  Optional<Pose3d> targetTagPose;
-  
+  private Pose3d tagFieldPosition;
+
+  private boolean isRedAlliance;
 
   int targetId;
 
   Field2d field = new Field2d();
 
-  // -0.523598776
-  static final Transform3d robotToCam = new Transform3d(
-      new Translation3d(0.0, 0, .4318),
-      new Rotation3d(0, 0, 0));
+  // TODO Adjust constant based on actual camera to robot height
+  // TODO: Add constant to shift to center of robot (or wherever needed)
+  Transform3d camToRobot = new Transform3d(
+    new Translation3d(0.0, 0, -.47),
+    new Rotation3d(0, 0, 0));
 
   public PhotonCameraSubsystem() {
     camera = new PhotonCamera("camera0");
     camera.setDriverMode(false);
     try {
-      layout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
+      NetworkTable fmsInfoTable = NetworkTableInstance.getDefault().getTable(FMSINFO_TABLE);
+      if (fmsInfoTable != null) {
+        NetworkTableEntry isRedAllianceEntry = fmsInfoTable.getEntry(IS_RED_ALLIANCE);
+        if (isRedAllianceEntry != null) {
+          isRedAlliance = isRedAllianceEntry.getBoolean(true);
+          layout = AprilTagMap.getAprilTagLayout(isRedAlliance);
+        }
+      } else {
+        layout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
+      }
     } catch (Exception e) {
       e.printStackTrace();
     }
 
-    estimator = new PhotonPoseEstimator(layout, PoseStrategy.LOWEST_AMBIGUITY, camera, robotToCam);
+    estimator = new PhotonPoseEstimator(layout, PoseStrategy.AVERAGE_BEST_TARGETS, camera, camToRobot);
 
     SmartDashboard.putData(field);
   }
 
-  // protected Pose2d calculatePoseEstimation() {
-  // Optional<EstimatedRobotPose> estimate = estimator.update();
-  // if (estimate.isPresent()) {
-  // EstimatedRobotPose pose = estimate.get();
-  // Pose3d poseFromEstimate = pose.estimatedPose;
+  private void checkAlliance() {
+    NetworkTable fmsInfoTable = NetworkTableInstance.getDefault().getTable(FMSINFO_TABLE);
 
-  // Pose2d robotPoseFrom3d = new Pose2d(
-  // poseFromEstimate.getX(),
-  // poseFromEstimate.getY(),
-  // new Rotation2d(poseFromEstimate.getRotation().getZ()));
+    if (fmsInfoTable != null) {
+      NetworkTableEntry isRedAllianceEntry = fmsInfoTable.getEntry(IS_RED_ALLIANCE);
 
-  // return robotPoseFrom3d;
-  // } else {
-  // System.out.println("No Tag Found");
-  // }
-  // return null;
-  // }
-
-  private void calculatePose2d() {
-    PhotonPipelineResult result = camera.getLatestResult();
-    boolean hasTargets = result.hasTargets();
-
-    if (hasTargets) {
-      PhotonTrackedTarget target = result.getBestTarget();
-
-      targetId = target.getFiducialId();
-      Transform3d targetLocation = target.getBestCameraToTarget();
-      targetTagPose = layout.getTagPose(targetId);
-      if (targetTagPose.isPresent()) {
-        Pose3d robotPosition = PhotonUtils.estimateFieldToRobotAprilTag(
-            targetLocation,
-            targetTagPose.get(),
-            robotToCam);
-
-        builtIn2D = robotPosition.toPose2d();
-      } else {
-        builtIn2D = null;
-      }
-    } else {
-      builtIn2D = null;
-      targetId = 0;
+      if (isRedAllianceEntry != null) {
+        boolean isRed = isRedAllianceEntry.getBoolean(false);
+        if (isRedAlliance != isRed) {
+          isRedAlliance = isRed;
+          layout = AprilTagMap.getAprilTagLayout(isRedAlliance);
+          estimator = new PhotonPoseEstimator(layout, PoseStrategy.AVERAGE_BEST_TARGETS, camera, camToRobot);
+        }
+      }  
     }
+    SmartShuffleboard.put("AprilTag", "isRedAlliance", isRedAlliance);
+  }
+
+  private Pose3d calculateUsingEstimator() {
+    Optional<EstimatedRobotPose> result = estimator.update();
+    if (result.isPresent()) {
+        EstimatedRobotPose estimatedPose = result.get();
+        targetId = estimatedPose.targetsUsed.get(0).getFiducialId();
+        tagFieldPosition = layout.getTagPose(targetId).get();
+        Pose3d pose = estimatedPose.estimatedPose;
+        return pose;
+    }
+    return null;
+  }
+
+  /**
+   * Gets the latest robotFieldPose, could be null
+   * @return
+   */
+  public Pose2d getRobot2dFieldPose() {
+    return robotFieldPose;
+  }
+
+  public Pose3d getRobotFieldPose() {
+    Pose3d result = calculateUsingEstimator();
+    if (result != null) {
+        robotFieldPose = result.toPose2d();
+        return result;
+    }
+    return null;
   }
 
   @Override
   public void periodic() {
-    SmartShuffleboard.put("AprilTag", "TestMessage", true);
-    calculatePose2d();
-    if (builtIn2D != null) {
+    checkAlliance();
+    Pose3d pose3dPosition = getRobotFieldPose();
+    if (robotFieldPose != null) {
+      SmartShuffleboard.put("AprilTag", "2D", "2D-X", robotFieldPose.getTranslation().getX());
+      SmartShuffleboard.put("AprilTag", "2D", "2D-Y", robotFieldPose.getTranslation().getY());
+      SmartShuffleboard.put("AprilTag", "2D", "Angle", robotFieldPose.getRotation().getDegrees());
+      SmartShuffleboard.put("AprilTag", "2D", "AngleR", robotFieldPose.getRotation().getRadians());
 
-      SmartShuffleboard.put("AprilTag", "2D", "2D-X", builtIn2D.getTranslation().getX());
-      SmartShuffleboard.put("AprilTag", "2D", "2D-Y", builtIn2D.getTranslation().getY());
-      SmartShuffleboard.put("AprilTag", "2D", "Angle", builtIn2D.getRotation().getDegrees());
-      SmartShuffleboard.put("AprilTag", "2D", "AngleR", builtIn2D.getRotation().getRadians());
-
-      // SmartShuffleboard.put("AprilTag", "RP", "X", robotPosition.getX());
-      // SmartShuffleboard.put("AprilTag", "RP", "Y", robotPosition.getY());
-      // SmartShuffleboard.put("AprilTag", "RP", "Z", robotPosition.getZ());
-      // SmartShuffleboard.put("AprilTag", "RP", "RX", robotPosition.getRotation().getX());
-      // SmartShuffleboard.put("AprilTag", "RP", "RY", robotPosition.getRotation().getY());
-      // SmartShuffleboard.put("AprilTag", "RP", "RZ", robotPosition.getRotation().getZ());
-
-
-      SmartShuffleboard.put("AprilTag", "tagposition-x", targetTagPose.get().getX());
-      SmartShuffleboard.put("AprilTag", "tagposition-y", targetTagPose.get().getY());
-
-      // SmartShuffleboard.put("AprilTag", "tagId", targetId);
-      // SmartShuffleboard.put("AprilTag", "x", targetLocation.getX());
-      // SmartShuffleboard.put("AprilTag", "y", targetLocation.getY());
-      // SmartShuffleboard.put("AprilTag", "z", targetLocation.getZ());
-
-      field.setRobotPose(builtIn2D);
-
+      field.setRobotPose(robotFieldPose);
     } else {
       SmartShuffleboard.put("AprilTag", "2D", "2D-X", 0);
       SmartShuffleboard.put("AprilTag", "2D", "2D-Y", 0);
@@ -135,10 +136,22 @@ public class PhotonCameraSubsystem extends SubsystemBase {
 
       field.setRobotPose(new Pose2d(-100, -100, new Rotation2d(0)));
     }
-  }
 
-  public Pose2d getPose2d() {
-    calculatePose2d();
-    return builtIn2D;
+    if (pose3dPosition != null) {
+        SmartShuffleboard.put("AprilTag", "3D", "3D-X", pose3dPosition.getX());
+        SmartShuffleboard.put("AprilTag", "3D", "3D-Y", pose3dPosition.getY());
+        SmartShuffleboard.put("AprilTag", "3D", "3D-Z", pose3dPosition.getZ());
+        SmartShuffleboard.put("AprilTag", "3D", "3D-RX", pose3dPosition.getRotation().getX()); 
+        SmartShuffleboard.put("AprilTag", "3D", "3D-RY", pose3dPosition.getRotation().getY()); 
+        SmartShuffleboard.put("AprilTag", "3D", "3D-RZ", pose3dPosition.getRotation().getZ());   
+    }
+
+    if (tagFieldPosition != null) {
+        SmartShuffleboard.put("AprilTag", "position-x", tagFieldPosition.getX());
+        SmartShuffleboard.put("AprilTag", "position-y", tagFieldPosition.getY());
+    } else {
+        SmartShuffleboard.put("AprilTag", "position-x", 0);
+        SmartShuffleboard.put("AprilTag", "position-y", 0);
+    }
   }
 }
