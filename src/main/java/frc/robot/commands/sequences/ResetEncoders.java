85ff9df54d9e3700a8378d3a56cb1ece1d46aaab
Sahiltheram <sahil.a.thadani@gmail.com>
2023-03-08 20:55:41 -0500

    hi

--- src/main/java/frc/robot/commands/sequences/ResetEncoders.java
+++ src/main/java/frc/robot/commands/sequences/ResetEncoders.java
@@ -16,6 +16,5 @@ public class ResetEncoders extends SequentialCommandGroup{
                 new ResetArm(arm).withTimeout(3),
                 new ResetArmEncoder(arm)
         );
-        
     }
 }
