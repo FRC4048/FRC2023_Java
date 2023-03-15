fcab992efbd45489078961fd63318f5685bc6800
Aayush <kumaraay25@student.westboroughk12.org>
2023-03-15 18:32:46 -0400

    camera server auto capture

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,8 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 import frc.robot.commands.*;
-
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -121,6 +122,8 @@ public class RobotContainer {
     putShuffleboardCommands();
 
     drivetrain.setDefaultCommand(new Drive(drivetrain, () -> joyLeft.getY(), () -> joyLeft.getX(), ()-> joyRight.getX(),joystickLeftButton));
+
+    UsbCamera camera = CameraServer.startAutomaticCapture(0);
   }
 
 
