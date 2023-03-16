8025d6c847f899f29837c5b52d3871779e53a8f5
Sushant Santhosh <sushant.santhosh26@gmail.com>
2023-03-16 18:30:57 -0400

    added maxSpeed param

--- src/main/java/frc/robot/commands/Autonomous/MoveDistanceOffset.java
+++ src/main/java/frc/robot/commands/Autonomous/MoveDistanceOffset.java
@@ -1,21 +1,20 @@
 package frc.robot.commands.Autonomous;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
 
 public class MoveDistanceOffset extends CommandBase{
     private Drivetrain drivetrain;
-    private double changeX, changeY, startPosX, startPosY, speedX, speedY, angle;
-    private boolean finished;
+    private double changeX, changeY, startPosX, startPosY, speedX, speedY, maxSpeed;
 
-    public MoveDistanceOffset(Drivetrain drivetrain, double changeX, double changeY, double speedX, double speedY, double speed) {
+    public MoveDistanceOffset(Drivetrain drivetrain, double changeX, double changeY, double speedX, double speedY, double speed, double maxSpeed) {
         this.drivetrain = drivetrain;
         this.changeX = changeX;
         this.changeY = changeY;
         this.speedX = speedX;
         this.speedY = speedY;
+        this.maxSpeed = maxSpeed;
     }
 
     @Override
@@ -24,11 +23,11 @@ public class MoveDistanceOffset extends CommandBase{
         startPosY = drivetrain.getPoseY();
 
         if(changeX > changeY) {
-            speedX = 0.7;
+            speedX = maxSpeed;
             speedY = speedX * (changeY/changeX);
         }
         else {
-            speedY = 0.7;
+            speedY = maxSpeed;
             speedX = speedY * (changeX/changeY);
         }
         
