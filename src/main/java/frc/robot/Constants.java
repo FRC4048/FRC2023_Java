fe2dcf7836df2a4e8743cb0ea7abf96c53674cb0
Aditya Hebbar <Adityarhebbar@gmail.com>
2023-03-16 20:20:55 -0400

    removed useless constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,7 +22,6 @@ public final class Constants {
   public static final boolean GRIPPER_DEBUG = false;
   public static final boolean PDB_DEBUG = false;
   public static final boolean APRILTAG_DEBUG = false;
-  public static final boolean APRILTAG_LOGGING = true;
   
   public static final boolean ENABLE_LOGGING = true;
 
