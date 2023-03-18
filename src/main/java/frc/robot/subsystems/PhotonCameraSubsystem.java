819dc5ce0683aded4b42e7a9d2751e1057f8692a
Aditya Hebbar <Adityarhebbar@gmail.com>
2023-03-18 15:16:57 -0400

    Implemented Filters

--- src/main/java/frc/robot/subsystems/PhotonCameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonCameraSubsystem.java
@@ -21,10 +21,29 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.apriltags.AprilTagMap;
+import frc.robot.apriltags.AprilTagPoseFilter;
 import frc.robot.utils.Logger;
 import frc.robot.utils.SmartShuffleboard;
 
 public class PhotonCameraSubsystem extends SubsystemBase {
+
+  private static final String FMSINFO_TABLE = "FMSInfo";
+  private static final String IS_RED_ALLIANCE = "IsRedAlliance";
+
+  private AprilTagPoseFilter x2DFilter = new AprilTagPoseFilter(3, 1); //Placeholder value in meters
+  private AprilTagPoseFilter y2DFilter = new AprilTagPoseFilter(3, 1); //Placeholder value in meters
+  private AprilTagPoseFilter angleFilter = new AprilTagPoseFilter(3, 0.15708); //Placeholder value in radians
+  private AprilTagPoseFilter angleRFilter = new AprilTagPoseFilter(3, 0.15708); //Placeholder value in radians
+  private AprilTagPoseFilter x3DFilter = new AprilTagPoseFilter(3, 1); //Placeholder value in meters
+  private AprilTagPoseFilter y3DFilter = new AprilTagPoseFilter(3, 1); //Placeholder value in meters
+  private AprilTagPoseFilter z3DFilter = new AprilTagPoseFilter(3, 1); //Placeholder value in meters
+  private AprilTagPoseFilter rX3DFilter = new AprilTagPoseFilter(3, 0.15708); //Placeholder value in radians
+  private AprilTagPoseFilter rY3DFilter = new AprilTagPoseFilter(3, 0.15708); //Placeholder value in radians
+  private AprilTagPoseFilter rZ3DFilter = new AprilTagPoseFilter(3, 0.15708); //Placeholder value in radians
+  private AprilTagPoseFilter positionXFilter = new AprilTagPoseFilter(3, 1); //Placeholder value in meters
+  private AprilTagPoseFilter positionYFilter = new AprilTagPoseFilter(3, 1); //Placeholder value in meters
+
+  private boolean useFilters = true;
 
   private PhotonCamera camera;
   private AprilTagFieldLayout layout;
@@ -153,12 +172,24 @@ public class PhotonCameraSubsystem extends SubsystemBase {
     if (Constants.APRILTAG_DEBUG) {
       SmartShuffleboard.put("AprilTag", "isConnected", camera.isConnected());
       if (robotFieldPose != null) {
-        SmartShuffleboard.put("AprilTag", "2D", "2D-X", robotFieldPose.getTranslation().getX());
-        SmartShuffleboard.put("AprilTag", "2D", "2D-Y", robotFieldPose.getTranslation().getY());
-        SmartShuffleboard.put("AprilTag", "2D", "Angle", robotFieldPose.getRotation().getDegrees());
-        SmartShuffleboard.put("AprilTag", "2D", "AngleR", robotFieldPose.getRotation().getRadians());
-
+        if (useFilters) {
+          SmartShuffleboard.put("AprilTag", "2D", "2D-X", x2DFilter.calculate(robotFieldPose.getTranslation().getX()));
+          SmartShuffleboard.put("AprilTag", "2D", "2D-Y", y2DFilter.calculate(robotFieldPose.getTranslation().getY()));
+          SmartShuffleboard.put("AprilTag", "2D", "Angle", angleFilter.calculate(robotFieldPose.getRotation().getDegrees()));
+          SmartShuffleboard.put("AprilTag", "2D", "AngleR", angleRFilter.calculate(robotFieldPose.getRotation().getRadians()));
+        }
+        else {
+          SmartShuffleboard.put("AprilTag", "2D", "2D-X", robotFieldPose.getTranslation().getX());
+          SmartShuffleboard.put("AprilTag", "2D", "2D-Y", robotFieldPose.getTranslation().getY());
+          SmartShuffleboard.put("AprilTag", "2D", "Angle", robotFieldPose.getRotation().getDegrees());
+          SmartShuffleboard.put("AprilTag", "2D", "AngleR", robotFieldPose.getRotation().getRadians());
+        }
+        
       } else {
+        x2DFilter.resetFilter();
+        y2DFilter.resetFilter();
+        angleFilter.resetFilter();
+        angleRFilter.resetFilter();
         SmartShuffleboard.put("AprilTag", "2D", "2D-X", 0);
         SmartShuffleboard.put("AprilTag", "2D", "2D-Y", 0);
         SmartShuffleboard.put("AprilTag", "2D", "Angle", 0);
@@ -167,20 +198,46 @@ public class PhotonCameraSubsystem extends SubsystemBase {
       }
       SmartShuffleboard.put("AprilTag", "noTagDetectedCounter", noTagDetectedCounter);
       if (pose3dPosition != null) {
-        SmartShuffleboard.put("AprilTag", "3D", "3D-X", pose3dPosition.getX());
-        SmartShuffleboard.put("AprilTag", "3D", "3D-Y", pose3dPosition.getY());
-        SmartShuffleboard.put("AprilTag", "3D", "3D-Z", pose3dPosition.getZ());
-        SmartShuffleboard.put("AprilTag", "3D", "3D-RX", pose3dPosition.getRotation().getX());
-        SmartShuffleboard.put("AprilTag", "3D", "3D-RY", pose3dPosition.getRotation().getY());
-        SmartShuffleboard.put("AprilTag", "3D", "3D-RZ", pose3dPosition.getRotation().getZ());
+        if (useFilters) {
+          SmartShuffleboard.put("AprilTag", "3D", "3D-X", x3DFilter.calculate(pose3dPosition.getX()));
+          SmartShuffleboard.put("AprilTag", "3D", "3D-Y", y3DFilter.calculate(pose3dPosition.getY()));
+          SmartShuffleboard.put("AprilTag", "3D", "3D-Z", z3DFilter.calculate(pose3dPosition.getZ()));
+          SmartShuffleboard.put("AprilTag", "3D", "3D-RX", rX3DFilter.calculate(pose3dPosition.getRotation().getX())); 
+          SmartShuffleboard.put("AprilTag", "3D", "3D-RY", rY3DFilter.calculate(pose3dPosition.getRotation().getY())); 
+          SmartShuffleboard.put("AprilTag", "3D", "3D-RZ", rZ3DFilter.calculate(pose3dPosition.getRotation().getZ()));
+        }
+        else {
+          SmartShuffleboard.put("AprilTag", "3D", "3D-X", pose3dPosition.getX());
+          SmartShuffleboard.put("AprilTag", "3D", "3D-Y", pose3dPosition.getY());
+          SmartShuffleboard.put("AprilTag", "3D", "3D-Z", pose3dPosition.getZ());
+          SmartShuffleboard.put("AprilTag", "3D", "3D-RX", pose3dPosition.getRotation().getX()); 
+          SmartShuffleboard.put("AprilTag", "3D", "3D-RY", pose3dPosition.getRotation().getY()); 
+          SmartShuffleboard.put("AprilTag", "3D", "3D-RZ", pose3dPosition.getRotation().getZ());
+        }   
+      }
+      else {
+        x3DFilter.resetFilter();
+        y3DFilter.resetFilter();
+        z3DFilter.resetFilter();
+        rX3DFilter.resetFilter();
+        rY3DFilter.resetFilter();
+        rZ3DFilter.resetFilter();
       }
 
       if (tagFieldPosition != null) {
-        SmartShuffleboard.put("AprilTag", "position-x", tagFieldPosition.getX());
-        SmartShuffleboard.put("AprilTag", "position-y", tagFieldPosition.getY());
+        if (useFilters) {
+          SmartShuffleboard.put("AprilTag", "position-x", positionXFilter.calculate(tagFieldPosition.getX()));
+          SmartShuffleboard.put("AprilTag", "position-y", positionYFilter.calculate(tagFieldPosition.getY()));
+        }
+        else {
+          SmartShuffleboard.put("AprilTag", "position-x", tagFieldPosition.getX());
+          SmartShuffleboard.put("AprilTag", "position-y", tagFieldPosition.getY());
+        }
       } else {
-        SmartShuffleboard.put("AprilTag", "position-x", 0);
-        SmartShuffleboard.put("AprilTag", "position-y", 0);
+         positionXFilter.resetFilter();
+          positionYFilter.resetFilter();
+          SmartShuffleboard.put("AprilTag", "position-x", 0);
+          SmartShuffleboard.put("AprilTag", "position-y", 0);
       }
     }
   }
