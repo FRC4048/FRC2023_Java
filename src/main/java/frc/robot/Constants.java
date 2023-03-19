11317160afa08cca82aa4e3cee0729bdc2d60b4b
bengold22 <98672229+bengold22@users.noreply.github.com>
2023-03-19 17:28:21 -0400

    Adjustment to P for auto balance

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -195,7 +195,7 @@ public final class Constants {
   public static final int BALANCE_PID_END = 50; //amount of cycles we want to be balanced for
   public static final int BALANCE_STEEP_END = 60;
   public static final double CHARGESTATION_TIMEOUT = 10;
-  public static final float BALANCE_kP = (float) 0.006;
+  public static final float BALANCE_kP = (float) 0.01;
   public static final double BALANCE_THRESH = 8.5; //consider balanced within +-2 degrees of 0
   public static final double BALANCE_STEEP = 10; //angle require for BalanceSteep() to end
   public static final double CROSS_END = 30;
