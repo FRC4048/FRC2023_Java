cca6595eeb04cbaacdc864db9beaacd411b71b22
Aditya Hebbar <Adityarhebbar@gmail.com>
2023-03-24 19:20:56 -0400

    Fixed bad change

--- src/main/java/frc/robot/commands/arm/ArmMoveSequence.java
+++ src/main/java/frc/robot/commands/arm/ArmMoveSequence.java
@@ -1,12 +1,12 @@
 package frc.robot.commands.arm;
 
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.extender.ExtendToPosition;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Extender;
-import frc.robot.utils.logging.wrappers.ParRaceCommandGroupWrapper;
+import frc.robot.utils.logging.wrappers.ParCommandGroupWrapper;
 
 /**
  * DO NOT CALL THIS DIRECTLY MUST BE WRAPPED IN A COMMAND
@@ -17,7 +17,7 @@ public class ArmMoveSequence extends SequentialCommandGroup {
         if (armTargetPosition > arm.getAnalogValue()) {
             addCommands(
                 new VoltageMoveArm(arm, Constants.ARM_AUTO_VOLTAGE_UP, Constants.ARM_AUTO_VOLTAGE_DOWN, armTargetPosition), 
-                new ParRaceCommandGroupWrapper(new ParallelRaceGroup(
+                new ParCommandGroupWrapper(new ParallelCommandGroup(
                     new HoldArmPID(arm,armTargetPosition),
                     new ExtendToPosition(extender,extenderTargetPosition)
                 ), "-Arm-Move-extend")
