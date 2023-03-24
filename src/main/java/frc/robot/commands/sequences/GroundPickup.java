13bddb37d720e9fbb668ff3c95abc276a9a73f02
Aditya Hebbar <Adityarhebbar@gmail.com>
2023-03-24 19:17:49 -0400

    Added for ground pickup

--- src/main/java/frc/robot/commands/sequences/GroundPickup.java
+++ src/main/java/frc/robot/commands/sequences/GroundPickup.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.sequences;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.arm.HoldArmPID;
@@ -10,16 +11,18 @@ import frc.robot.commands.gripper.OpenGripper;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Extender;
 import frc.robot.subsystems.GripperSubsystem;
-import frc.robot.utils.logging.wrappers.ParCommandGroupWrapper;
+import frc.robot.utils.logging.wrappers.ParRaceCommandGroupWrapper;
 
 public class GroundPickup extends SequentialCommandGroup{
     public GroundPickup(Arm arm, Extender extender, GripperSubsystem gripper) {
         setName("-Ground-Pickup");
         addCommands(
             new VoltageMoveArm(arm, Constants.ARM_AUTO_VOLTAGE_UP, Constants.ARM_AUTO_VOLTAGE_DOWN, Constants.GROUND_PICKUP_ANGLE),
-            new ParCommandGroupWrapper(new ParallelCommandGroup(
+            new ParRaceCommandGroupWrapper(new ParallelRaceGroup(
+                new ParallelCommandGroup(
                 new ExtendToPosition(extender, 2000.0),
-                new OpenGripper(gripper),
+                new OpenGripper(gripper)
+                ),
                 new HoldArmPID(arm, Constants.GROUND_PICKUP_ANGLE)
             ), "-Ground-Pickup-drop")
         );
