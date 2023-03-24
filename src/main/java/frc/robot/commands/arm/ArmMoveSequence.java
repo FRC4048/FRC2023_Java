e03ee5176ff02897e7c2c2c7aaef017e29624a5b
Aditya Hebbar <Adityarhebbar@gmail.com>
2023-03-24 19:08:39 -0400

    Made it so that most Hold arm pid commands are in Parallel race groups - No logs yet

--- src/main/java/frc/robot/commands/arm/ArmMoveSequence.java
+++ src/main/java/frc/robot/commands/arm/ArmMoveSequence.java
@@ -1,19 +1,12 @@
 package frc.robot.commands.arm;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.ArmPositionGrid;
 import frc.robot.Constants;
-import frc.robot.RobotContainer;
 import frc.robot.commands.extender.ExtendToPosition;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Extender;
-import frc.robot.subsystems.PieceGrid;
-import frc.robot.subsystems.ProtectionMechanism;
-import frc.robot.utils.SmartShuffleboard;
-import frc.robot.utils.logging.wrappers.ParCommandGroupWrapper;
+import frc.robot.utils.logging.wrappers.ParRaceCommandGroupWrapper;
 
 /**
  * DO NOT CALL THIS DIRECTLY MUST BE WRAPPED IN A COMMAND
@@ -24,7 +17,7 @@ public class ArmMoveSequence extends SequentialCommandGroup {
         if (armTargetPosition > arm.getAnalogValue()) {
             addCommands(
                 new VoltageMoveArm(arm, Constants.ARM_AUTO_VOLTAGE_UP, Constants.ARM_AUTO_VOLTAGE_DOWN, armTargetPosition), 
-                new ParCommandGroupWrapper(new ParallelCommandGroup(
+                new ParRaceCommandGroupWrapper(new ParallelRaceGroup(
                     new HoldArmPID(arm,armTargetPosition),
                     new ExtendToPosition(extender,extenderTargetPosition)
                 ), "-Arm-Move-extend")
--- src/main/java/frc/robot/commands/sequences/SubstationPickup.java
+++ src/main/java/frc/robot/commands/sequences/SubstationPickup.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.sequences;
 
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.commands.LookForSubstation;
@@ -16,16 +17,20 @@ public class SubstationPickup extends SequentialCommandGroup {
     public SubstationPickup(Arm arm, GripperSubsystem gripper, Extender extender) {
         addCommands(
             new VoltageMoveArm(arm, Constants.ARM_AUTO_VOLTAGE_UP, Constants.ARM_AUTO_VOLTAGE_DOWN, Constants.SUBSTATION_PICKUP_ANGLE),
-            new HoldArmPID(arm, Constants.SUBSTATION_PICKUP_ANGLE),
-                new SequentialCommandGroup(
-                    new ExtendToPosition(extender, Constants.SUBSTATION_PICKUP_EXTENSION),
-                    new OpenGripper(gripper),
-                    new LookForSubstation()         
-                ),
-                new SequentialCommandGroup(
-                    new CloseGripper(gripper),
-                    new VoltageMoveArm(arm, Constants.ARM_AUTO_VOLTAGE_UP, Constants.ARM_AUTO_VOLTAGE_DOWN, Constants.SUBSTATION_POST_PICKUP_ANGLE)
+            new ParallelRaceGroup(
+                new HoldArmPID(arm, Constants.SUBSTATION_PICKUP_ANGLE),
+                new SequentialCommandGroup( 
+                    new SequentialCommandGroup(
+                        new ExtendToPosition(extender, Constants.SUBSTATION_PICKUP_EXTENSION),
+                        new OpenGripper(gripper),
+                        new LookForSubstation()         
+                    ),
+                    new SequentialCommandGroup(
+                        new CloseGripper(gripper),
+                        new VoltageMoveArm(arm, Constants.ARM_AUTO_VOLTAGE_UP, Constants.ARM_AUTO_VOLTAGE_DOWN, Constants.SUBSTATION_POST_PICKUP_ANGLE)
+                    )
                 )
+            )
         );
     }
 }
