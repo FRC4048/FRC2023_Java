1d2cd0d6fb2d53a5de6c56d1f311ca201b82ce84
bengold22 <98672229+bengold22@users.noreply.github.com>
2023-04-05 16:03:11 -0400

    Updated setpoints

--- src/main/java/frc/robot/ArmPositionGrid.java
+++ src/main/java/frc/robot/ArmPositionGrid.java
@@ -1,19 +1,18 @@
 package frc.robot;
 
-
 public enum ArmPositionGrid {
-     TOP_LEFT(39.0,7410),
-     TOP_MIDDLE(36.0,6355),
-     TOP_RIGHT(39.0,7410),
-     MIDDLE_LEFT(34.2,3570),
-     MIDDLE_MIDDLE(30.0,2865),
-     MIDDLE_RIGHT(34.2,3570),
+     TOP_LEFT(34.5,7410),
+     TOP_MIDDLE(30.5,7000),
+     TOP_RIGHT(34.5,7410),
+     MIDDLE_LEFT(29.0,3570),
+     MIDDLE_MIDDLE(26.5,2965),
+     MIDDLE_RIGHT(29.0,3570),
      DOWN_LEFT(14.5,3300),
      DOWN_MIDDLE(14.5,3300),
      DOWN_RIGHT(14.5,3300),
      GROUND_PICKUP(16.0, 4600),
      SUBSTATION_PICKUP(28.5, 2000.0),
-     SUBSTATION_POST_PICKUP(34.5, 2000.0),
+     SUBSTATION_POST_PICKUP(30.5, 2000.0),
      STOW(0,50);
 
      private final double armPosition;
